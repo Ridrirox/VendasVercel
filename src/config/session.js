efa30185c2bfa3c0256915046f80fb5454715522
Rodrigo <digaotekwondogarras@gmail.com>
2023-06-29 22:33:26 -0300

    test

---
+++ src/config/session.js
@@ -0,0 +1,19 @@
+const session = require("express-session");
+const MongoDBStore = require("connect-mongodb-session")(session);
+
+module.exports = session({
+  store: new MongoDBStore({
+    uri: process.env.DB_URL,
+   // databaseName: "cms",
+   // collection: "mySessions",
+	
+	databaseName: "sistema",
+    collection: "users",
+  }),
+  secret: "secretsessiontest",
+  resave: false,
+  saveUninitializad: false,
+  cookie: {
+    maxAge: 30 * 24 * 60 * 60 * 1000,
+  },
+});
